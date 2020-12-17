0487df6e5cf23cf21fd2bbe2b0ebcc130724ff33
Marius <grausas@gmail.com>
2020-12-17 15:21:30 +0200

    student data to form added

--- src/utils/FormData.js
+++ src/utils/FormData.js
@@ -4,8 +4,8 @@ export const LoginUser = [
     type: "email",
     labelText: "Email",
     required: true,
-    minLength: 2,
-    maxLEngth: 32,
+    minLength: 6,
+    maxLEngth: 256,
     placeholder: "jonas@email.lt",
   },
   {
@@ -13,8 +13,29 @@ export const LoginUser = [
     type: "password",
     labelText: "Password",
     required: true,
-    minLength: 2,
-    maxLEngth: 32,
+    minLength: 8,
+    maxLength: 64,
     placeholder: "password",
+  },
+];
+
+export const addStudent = [
+  {
+    name: "name",
+    type: "text",
+    labelText: "Name",
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+    placeholder: "Jonas",
+  },
+  {
+    name: "surname",
+    type: "text",
+    labelText: "Surname",
+    required: true,
+    minLength: 2,
+    maxLEngth: 50,
+    placeholder: "Jonaitis",
   },
 ];
