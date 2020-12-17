0384cd28b947a9f7dddf29b66c75d18c707be010
Marius <grausas@gmail.com>
2020-12-17 17:07:21 +0200

    notification to login and add student pages added

--- src/pages/AddStudent/AddStudent.jsx
+++ src/pages/AddStudent/AddStudent.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Section, Form } from "../../components";
+import React, { useState } from "react";
+import { Section, Form, Notification } from "../../components";
 import * as S from "./AddStudent.style";
 import { addStudent } from "../../utils/FormData";
 
-function addStudentData(data) {
+function addStudentData(data, setError, setType, error) {
   fetch("http://localhost:8080/students", {
     method: "POST",
     headers: {
@@ -12,25 +12,46 @@ function addStudentData(data) {
     body: JSON.stringify(data),
   })
     .then((res) => {
+      if (!res.ok) {
+        error = true;
+      } else {
+        error = false;
+      }
       return res.json();
     })
+    .then((data) => {
+      if (error) {
+        setType("error");
+        setError(data.msg);
+      } else {
+        setType("");
+        setError(data.msg);
+      }
+    })
     .catch((err) => {
-      console.log(err.message);
+      setError(err.message);
+      setType("error");
     });
 }
 
 function AddStudent() {
+  const [error, setError] = useState();
+  const [type, setType] = useState();
+
   return (
     <Section>
       <h2>Add Student</h2>
       <S.FormWrapper>
         <Form
-          callback={(fieldValues) => addStudentData(fieldValues)}
+          callback={(fieldValues) =>
+            addStudentData(fieldValues, setError, setType)
+          }
           fields={addStudent}
           titleText="Add Student"
           buttonText="Add Student"
         />
       </S.FormWrapper>
+      {error && <Notification type={type}>{error}</Notification>}
     </Section>
   );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Section, Form } from "../../components/index";
+import { Section, Form, Notification } from "../../components/index";
 import { LoginUser } from "../../utils/FormData";
 
-function signUser(userData, history) {
+function signUser(userData, setError, setType, history, error) {
   fetch("http://localhost:8080/login", {
     method: "POST",
     headers: {
@@ -11,17 +11,41 @@ function signUser(userData, history) {
     },
     body: JSON.stringify(userData),
   })
-    .then((res) => res.json())
-    .then(() => history.push("/"));
+    .then((res) => {
+      if (!res.ok) {
+        error = true;
+        setType("error");
+      } else {
+        error = false;
+        setType("error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      if (error) {
+        setError(data.msg || "Error!");
+      } else {
+        history.push("/");
+      }
+    })
+    .catch((err) => {
+      setError(err.message);
+      setType("error");
+    });
 }
 
 function Login() {
   const history = useHistory();
+  const [error, setError] = useState();
+  const [type, setType] = useState();
 
   return (
     <Section>
+      {error && <Notification type={type}>{error}</Notification>}
       <Form
-        callback={(fieldValues) => signUser(fieldValues, history)}
+        callback={(fieldValues) =>
+          signUser(fieldValues, setError, setType, history)
+        }
         fields={LoginUser}
         titleText="Login"
         buttonText="Login"
