8b5809747786b8e5c4bcf10fbce4c3705f1307b3
Marius <grausas@gmail.com>
2020-12-17 16:52:13 +0200

    students page with fetch added

--- src/pages/Students/Students.jsx
+++ src/pages/Students/Students.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { Section } from "../../components/index";
+import React, { useEffect, useState } from "react";
+import { Section, Table } from "../../components/index";
+import { tableStudents } from "../../utils/TableData";
 
 function Students() {
+  const [students, setStudents] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/view-students")
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+  }, []);
   return (
     <Section>
       <h2>Students</h2>
+      <Table cols={tableStudents} data={students} tableTitle="Students" />
     </Section>
   );
 }
