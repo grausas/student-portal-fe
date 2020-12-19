8531cb5ab6fe5381319ffb543ac8605d2f3a5dff
Marius <grausas@gmail.com>
2020-12-19 16:07:39 +0200

    added delete to students table

--- src/pages/Students/Students.jsx
+++ src/pages/Students/Students.jsx
@@ -5,15 +5,36 @@ import { tableStudents } from "../../utils/TableData";
 function Students() {
   const [students, setStudents] = useState();
 
+  const handleDelete = (item) => () => {
+    const itemId = item.id;
+    window.confirm("Do you want to delete this item?") &&
+      fetch(`http://localhost:8080/delete/${itemId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+      }).then((response) => {
+        if (!response.ok) {
+          throw response;
+        } else {
+          setStudents(students.filter((item) => itemId !== item.id));
+        }
+      });
+  };
   useEffect(() => {
     fetch("http://localhost:8080/view-students")
       .then((res) => res.json())
       .then((data) => setStudents(data));
   }, []);
+
   return (
     <Section>
       <h2>Students</h2>
-      <Table cols={tableStudents} data={students} tableTitle="Students" />
+      <Table
+        cols={tableStudents(handleDelete)}
+        data={students}
+        tableTitle="Students"
+      />
     </Section>
   );
 }
--- src/utils/TableData.js
+++ src/utils/TableData.js
@@ -1,23 +1,45 @@
-export const tableStudents = [
-  {
-    title: "Name",
-    render: (rowData) => {
-      return rowData.name;
+import React from "react";
+import * as FaIcons from "react-icons/ai";
+
+export const tableStudents = (handleDelete) => {
+  return [
+    {
+      title: "Name",
+      render: (rowData) => {
+        return rowData.name;
+      },
     },
-  },
-  {
-    title: "Surname",
-    render: (rowData) => {
-      return rowData.surname;
+    {
+      title: "Surname",
+      render: (rowData) => {
+        return rowData.surname;
+      },
     },
-  },
-  {
-    title: "Email",
-    render: (rowData) => {
-      return rowData.email;
+    {
+      title: "Email",
+      render: (rowData) => {
+        return rowData.email;
+      },
     },
-  },
-];
+    {
+      title: "Delete",
+      render: (rowData) => {
+        return (
+          <span onClick={handleDelete(rowData)}>
+            <FaIcons.AiFillDelete
+              style={{
+                color: "red",
+                fontSize: "1.3em",
+                padding: "0",
+                cursor: "pointer",
+              }}
+            />
+          </span>
+        );
+      },
+    },
+  ];
+};
 
 export const tableCourse = [
   {
@@ -41,21 +63,22 @@ export const tableCourse = [
   {
     title: "Group",
     render: (rowData) => {
-      return rowData.id;
+      return rowData.groupId;
     },
   },
 ];
+
 export const tableGroup = [
   {
     title: "Group",
     render: (rowData) => {
-      return rowData.id;
+      return rowData.groupid;
     },
   },
   {
     title: "Student",
     render: (rowData) => {
-      return rowData.name + " " + rowData.surname;
+      return rowData.student;
     },
   },
 ];
