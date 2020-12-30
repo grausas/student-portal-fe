e7442e45888c50c65f9f0c4bad18c647734a433b
Marius <grausas@gmail.com>
2020-12-30 17:41:55 +0200

    global style and container changed

--- src/components/Container/Container.style.js
+++ src/components/Container/Container.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1080px;
+  max-width: 1280px;
   margin: 0 auto;
   display: flex;
   box-sizing: border-box;
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -12,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
       background-image: url(${logo});
       background-size: cover;
       background-repeat: no-repeat;
-        /* background: #eef5f9; */
+      background-attachment: fixed;
    }
 `;
 
