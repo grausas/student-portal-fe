fdc2286cec26d80ef8c34a09e20a691fb4730350
Marius <grausas@gmail.com>
2021-01-05 18:50:27 +0200

    overflow fixed

--- src/components/Section/Section.style.js
+++ src/components/Section/Section.style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section`
   background-color: #eef5f9;
   width: 100%;
-  padding: 4em 1em;
+  padding: 3em 1em;
   margin-left: 220px;
   @media only screen and (max-width: 768px) {
     margin-left: 0;
@@ -13,6 +13,8 @@ export const Section = styled.section`
 export const Container = styled.section`
   height: 100%;
   width: 100%;
+  overflow: auto;
+  padding: 1em 0;
 
   @media only screen and (max-width: 768px) {
     width: 100%;
