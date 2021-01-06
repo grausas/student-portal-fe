b7986e3aae23689b536c5b20307a573dd7e6ab52
Marius <grausas@gmail.com>
2021-01-06 12:22:51 +0200

    Modal component init

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Modal.style";
+
+function Modal() {
+  return (
+    <div>
+      <h2>Modal</h2>
+    </div>
+  );
+}
+
+export default Modal;
--- src/components/index.js
+++ src/components/index.js
@@ -10,3 +10,4 @@ export { default as Container } from "./Container/Container";
 export { default as Footer } from "./Footer/Footer";
 export { default as Notification } from "./Notification/Notification";
 export { default as PrivateRoute } from "./PrivateRoute/PrivateRoute";
+export { default as Modal } from "./Modal/Modal";
