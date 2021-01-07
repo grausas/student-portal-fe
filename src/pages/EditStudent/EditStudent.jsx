c0bdd9ad1576d4e26bb7240255a81452c0b4f99f
Marius <grausas@gmail.com>
2021-01-07 19:51:25 +0200

    get student data from students page

--- src/pages/EditStudent/EditStudent.jsx
+++ src/pages/EditStudent/EditStudent.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Section } from "../../components";
+import { useLocation } from "react-router-dom";
 
 function EditStudent() {
+  const location = useLocation();
+  const [editStudent, setEditStudent] = useState({});
+
+  useEffect(() => {
+    const studentData = location.state.detail;
+    setEditStudent({
+      id: studentData.id,
+      name: studentData.fullname.split(" ")[0],
+      surname: studentData.fullname.split(" ")[1],
+      email: studentData.email,
+      phone: studentData.phone,
+    });
+  }, [location]);
+
   return (
     <Section>
       <h2>Edit Student</h2>
+      <li> {editStudent.name} </li>
     </Section>
   );
 }
