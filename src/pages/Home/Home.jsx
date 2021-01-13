07b1b62071df58a26f589d4370d4c97e5e3c4764
Marius <grausas@gmail.com>
2021-01-13 21:25:42 +0200

    created home data file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,9 @@
 import React from "react";
 import { Section, Card } from "../../components";
 import * as S from "./Home.style";
-import students from "../../assets/students.png";
-import teacher from "../../assets/teacher.png";
-import graduate from "../../assets/graduate.png";
+import { homeData } from "../../utils/HomeData";
 
 function Home() {
-  const homeData = [
-    {
-      name: "Students",
-      image: students,
-      info: "23",
-    },
-    {
-      name: "Teachers",
-      image: teacher,
-      info: "5",
-    },
-    {
-      name: "Graduates",
-      image: graduate,
-      info: "20",
-    },
-  ];
-
   return (
     <>
       <Section>
---
+++ src/utils/HomeData.js
@@ -0,0 +1,21 @@
+import students from "../assets/students.png";
+import teacher from "../assets/teacher.png";
+import graduate from "../assets/graduate.png";
+
+export const homeData = [
+  {
+    name: "Students",
+    image: students,
+    info: "23",
+  },
+  {
+    name: "Teachers",
+    image: teacher,
+    info: "5",
+  },
+  {
+    name: "Graduates",
+    image: graduate,
+    info: "20",
+  },
+];
