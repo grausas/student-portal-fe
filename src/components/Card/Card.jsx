6358beec069a30c0fe41e5b4ac8cb1c19c2403fd
Marius <grausas@gmail.com>
2021-01-13 17:42:46 +0200

    card component design changed

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import * as S from "./Card.style";
 
-function Card({ array }) {
+function Card({ data }) {
   return (
     <>
-      <S.CarBlock>
-        <S.CarImage />
-        <S.CarName>Card</S.CarName>
-      </S.CarBlock>
+      {data &&
+        data.map((item, index) => (
+          <S.CardBlock key={index}>
+            <S.CardImage src={item.image} />
+            <div>
+              <S.CardTitle>{item.name}</S.CardTitle>
+              <S.CardInfo> 22 </S.CardInfo>
+            </div>
+          </S.CardBlock>
+        ))}
     </>
   );
 }
--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -1,17 +1,29 @@
 import styled from "styled-components";
 
-export const CarBlock = styled.div`
+export const CardBlock = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   box-sizing: border-box;
+  background: #fff;
+  width: 350px;
   margin-bottom: 20px;
-  padding: 10px;
+  padding: 1em;
+  border-radius: 10px;
   border: 2px solid #eee;
+  text-align: center;
 `;
 
-export const CarImage = styled.img``;
+export const CardImage = styled.img`
+  max-width: 150px;
+`;
 
-export const CarName = styled.div`
+export const CardTitle = styled.div`
   font-size: 1.2em;
   margin-top: 10px;
+`;
+
+export const CardInfo = styled.div`
+  background: #eee;
+  padding: 20px;
+  border-radius: 10px;
 `;
