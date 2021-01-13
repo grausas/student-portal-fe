c7a1e9c90d00a6e02f31494d32c5c86baf76ce89
Marius <grausas@gmail.com>
2021-01-13 18:27:05 +0200

    card imported to home page and data created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,36 @@
 import React from "react";
-import { Section } from "../../components";
+import { Section, Card } from "../../components";
 import * as S from "./Home.style";
+import students from "../../assets/students.png";
+import teacher from "../../assets/teacher.png";
+import graduate from "../../assets/graduate.png";
 
 function Home() {
+  const homeData = [
+    {
+      name: "Students",
+      image: students,
+      info: "23",
+    },
+    {
+      name: "Teachers",
+      image: teacher,
+      info: "5",
+    },
+    {
+      name: "Graduates",
+      image: graduate,
+      info: "20",
+    },
+  ];
+
   return (
     <>
       <Section>
         <h1>Welcome to student's portal</h1>
-        <S.FlexDiv></S.FlexDiv>
+        <S.FlexDiv>
+          <Card data={homeData} />
+        </S.FlexDiv>
       </Section>
     </>
   );
